8ac58deed0a417bd44e766a39da1d4fd7fa3d4cb
Dan Carmon <dan.carmon@trusteer.com>
2014-04-26 19:38:55 +0300

    Initial functionaility

--- volumen-server.rb
+++ volumen-server.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+
+set :port, 8080
 set :public_folder, File.dirname(__FILE__)
-post '/set'do 
+get '/volume' do
+  output = `amixer get Master`
+  volume = output.match(/\[([0-9]{1,3})%\]/)
+  volume && volume[1] || "0"
+end
+
+post '/volume' do
   #raise "Invalid volume: #{volume}" unless volume.between
   puts "request['volume']:", request["volume"]
   volume = request["volume"].to_i rescue nil
@@ -11,6 +19,5 @@ post '/set'do
 end
 
 get '/' do
-  redirect 'index.html' 
+  redirect 'index.html'
 end
-
